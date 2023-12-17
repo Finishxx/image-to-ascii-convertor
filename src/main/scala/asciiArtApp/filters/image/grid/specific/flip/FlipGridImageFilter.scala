6f0e0252b558be6ebd264d83a16bb33fbf134081
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-17 14:32:22 +0100

    Make FlipGridImageFilter take type params

--- src/main/scala/asciiArtApp/filters/image/grid/specific/flip/FlipGridImageFilter.scala
+++ src/main/scala/asciiArtApp/filters/image/grid/specific/flip/FlipGridImageFilter.scala
@@ -4,5 +4,5 @@ import asciiArtApp.filters.image.grid.specific.Seq2DToGridImageFilterAdapter
 import filters.grid.flip.Axis.Axis
 import filters.grid.flip.FlipSeq2DFilter
 
-class FlipGridImageFilter(axis: Axis)
-    extends Seq2DToGridImageFilterAdapter(new FlipSeq2DFilter(axis))
+class FlipGridImageFilter[T](axis: Axis)
+    extends Seq2DToGridImageFilterAdapter[T](new FlipSeq2DFilter(axis))
