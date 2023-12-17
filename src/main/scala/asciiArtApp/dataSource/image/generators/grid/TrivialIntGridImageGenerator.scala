f654358e49615e949dc432f351a00b17908119cd
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-17 14:17:54 +0100

    Make TrivialIntGrid Generator depend on some sort of Int generator

--- src/main/scala/asciiArtApp/dataSource/image/generators/grid/TrivialIntGridImageGenerator.scala
+++ src/main/scala/asciiArtApp/dataSource/image/generators/grid/TrivialIntGridImageGenerator.scala
@@ -1,13 +1,17 @@
 package asciiArtApp.dataSource.image.generators.grid
 
 import asciiArtApp.model.image.grid.{GridImage, GridImageAs2DSeq}
+import dataSource.generators.number.IntGenerator
 
 import scala.util.Random
 
-class TrivialIntGridImageGenerator(width: Int, height: Int)
+class TrivialIntGridImageGenerator(
+  width: Int,
+  height: Int,
+  intGenerator: IntGenerator)
     extends GridImageGenerator[Int](width, height) {
   override def generate(width: Int, height: Int): GridImage[Int] = {
     val random: Random = new Random
-    new GridImageAs2DSeq(Seq.fill(width, height)(random.nextInt()))
+    new GridImageAs2DSeq(Seq.fill(width, height)(intGenerator.provide()))
   }
 }
