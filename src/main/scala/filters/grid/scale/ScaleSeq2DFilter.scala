fabc38be6d8be57335081223b55115c6793cc647
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-18 16:13:42 +0100

    Fix ScaleSeq2DFilter not scaling vertically and scaling by weird factor

--- src/main/scala/filters/grid/scale/ScaleSeq2DFilter.scala
+++ src/main/scala/filters/grid/scale/ScaleSeq2DFilter.scala
@@ -5,15 +5,17 @@ import filters.grid.Seq2DFilter
 class ScaleSeq2DFilter[T](factor: Double) extends Seq2DFilter[T] {
 
   private def quadruple(grid: Seq[Seq[T]]): Seq[Seq[T]] =
-    grid.map(_.flatMap(Seq.fill(4)(_)))
+    grid.flatMap(line => Seq.fill(2)(line.flatMap(Seq.fill(2)(_))))
 
   private def quarter(grid: Seq[Seq[T]]): Seq[Seq[T]] =
-    grid.map(_.grouped(4).map(_.head).toSeq)
+    grid.grouped(2).map(_.head).toSeq.map(_.grouped(2).map(_.head).toSeq)
 
   override def filter(what: Seq[Seq[T]]): Seq[Seq[T]] = factor match {
     case 1    => what
     case 4    => quadruple(what)
     case 0.25 => quarter(what)
-    case _    => throw new IllegalArgumentException("Invalid rotation angle")
+    case _ =>
+      throw new IllegalArgumentException(
+        s"Invalid scaling factor! Expected 0.25, 1 or 4, but received $factor")
   }
 }
