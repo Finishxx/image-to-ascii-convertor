e9787e1913386bc1cc5730981b70e2b9cd4afd94
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-18 21:21:34 +0100

    Get rid of Inner converter

--- src/main/scala/asciiArtApp/converters/image/InnerGridImageConverter.scala
+++ src/main/scala/asciiArtApp/converters/image/InnerGridImageConverter.scala
@@ -2,12 +2,9 @@ package asciiArtApp.converters.image
 
 import asciiArtApp.model.image.grid.GridImage
 import converters.Converter
-import converters.specific.InnerConverter
 
 class InnerGridImageConverter[T, V](converter: Converter[T, V])
-    extends InnerConverter[GridImage, T, V](converter) {
-  override def convertInner(
-    what: GridImage[T],
-    conversion: T => V): GridImage[V] =
-    what.map(conversion)
+    extends Converter[GridImage[T], GridImage[V]] {
+  override def convert(what: GridImage[T]): GridImage[V] =
+    what.map(converter.convert)
 }
