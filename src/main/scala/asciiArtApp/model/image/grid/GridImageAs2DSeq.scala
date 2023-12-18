787ddaf65a305278fc38ebfc280f9c80ad06bb08
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-18 22:17:53 +0100

    Add requires to `at` in GridImageAs2DSeq

--- src/main/scala/asciiArtApp/model/image/grid/GridImageAs2DSeq.scala
+++ src/main/scala/asciiArtApp/model/image/grid/GridImageAs2DSeq.scala
@@ -9,7 +9,11 @@ class GridImageAs2DSeq[T](grid: Seq[Seq[T]]) extends GridImage[T] {
 
   override def height: Int = grid.head.size
 
-  override def at(x: Int, y: Int): T = grid(x - 1)(y - 1)
+  override def at(x: Int, y: Int): T = {
+    require((1 to width).contains(x), "GridImage accessed with invalid x!")
+    require((1 to height).contains(y), "GridImage accessed with invalid y!")
+    grid(x - 1)(y - 1)
+  }
 
   override def map[V](fun: T => V): GridImage[V] =
     new GridImageAs2DSeq(grid.map(_.map(fun)))
