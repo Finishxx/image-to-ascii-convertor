5ffd5ee7c22cb4a0fc0255d7b25c52df7bb86678
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-19 11:39:17 +0100

    Add BufferedImageImporter tests

--- src/test/scala/asciiArtApp/dataSource/image/imageIo/BufferedImageImporterTest.scala
+++ src/test/scala/asciiArtApp/dataSource/image/imageIo/BufferedImageImporterTest.scala
@@ -1,5 +1,47 @@
 package asciiArtApp.dataSource.image.imageIo
 
-class BufferedImageImporterTest {
+import helpers.TestWithImages
+import org.scalatest.FunSuite
+import org.scalatest.Matchers.{an, be, noException}
+
+import java.awt.image.BufferedImage
+import java.io.File
+
+class BufferedImageImporterTest extends FunSuite with TestWithImages {
+
+  def printBufferedImageContents(bufferedImage: BufferedImage): Unit = {
+    val intArray: Array[Int] = bufferedImage.getRGB(
+      0,
+      0,
+      bufferedImage.getWidth,
+      bufferedImage.getHeight,
+      null,
+      0,
+      bufferedImage.getWidth)
+    print(intArray.map(_.toOctalString).mkString("Array(", ", ", ")"))
+  }
+
+  def loadFile(fileName: String): Unit = {
+    val importer = new BufferedImageImporter(new File(fileName))
+    noException should be thrownBy importer.provide()
+    // printBufferedImageContents(importer.provide())
+  }
+
+  test("Nonexistent file") {
+    an[IllegalArgumentException] should be thrownBy new BufferedImageImporter(
+      new File("asdasedgfgfddfdfjhgdfgdsfjgsdjlkghsdfhdfg"))
+  }
+
+  test("We can load gif") {
+    loadFile(white4x4Gif)
+  }
+
+  test("We can load png") {
+    loadFile(white4x4Png)
+  }
+
+  test("We can load jpg") {
+    loadFile(white4x4Jpg)
+  }
 
 }
---
+++ src/test/scala/helpers/TestWithImages.scala
@@ -0,0 +1,9 @@
+package helpers
+
+trait TestWithImages {
+
+  def white4x4Gif: String = "src/test/scala/testFiles/4x4white.gif"
+  def white4x4Jpg: String = "src/test/scala/testFiles/4x4white.jpg"
+  def white4x4Png: String = "src/test/scala/testFiles/4x4white.png"
+
+}
