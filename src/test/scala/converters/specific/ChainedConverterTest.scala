436efb7710c903cb0348dc746413901a7545c9fd
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-19 15:45:54 +0100

    Can't think of anything more interesting to test here, maybe other converters might add spice here

--- src/test/scala/converters/specific/ChainedConverterTest.scala
+++ src/test/scala/converters/specific/ChainedConverterTest.scala
@@ -1,5 +1,39 @@
 package converters.specific
 
 import org.scalatest.FunSuite
+import org.scalatest.Matchers.{an, be, convertToAnyShouldWrapper, equal}
 
-class ChainedConverterTest extends FunSuite {}
+class ChainedConverterTest extends FunSuite {
+
+  test("Substitute there and back should be identity") {
+    def testThereAndBackSubstitution[K, V](thereMap: Map[K, V]): Unit = {
+      val backMap = thereMap.toSeq.map(_.swap).toMap
+
+      val thereConverter: SubstitutionConverter[K, V] =
+        new SubstitutionConverter(thereMap)
+      val backConverter: SubstitutionConverter[V, K] =
+        new SubstitutionConverter(backMap)
+
+      val chainedConverter = new ChainedConverter(thereConverter, backConverter)
+
+      for ((k, _) <- thereMap)
+        chainedConverter.convert(k) should equal(k)
+    }
+
+    testThereAndBackSubstitution(
+      Map(1 -> "one", 2 -> "two", 3 -> "three", 4 -> "four"))
+
+    testThereAndBackSubstitution(
+      Map(
+        "hello" -> "world",
+        "it is" -> "cloudy today",
+        "feels like" -> "christmas"))
+
+    testThereAndBackSubstitution(
+      Seq.range(1, 100).map(int => (int, int.toString)).toMap)
+
+    testThereAndBackSubstitution(
+      Seq.range(1, 100, 2).map(int => (int, int)).toMap)
+  }
+
+}
