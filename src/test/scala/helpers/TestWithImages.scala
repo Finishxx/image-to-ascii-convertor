11f5806ac7b51298339bd4118c54ae3cd2e3baf7
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-19 16:23:45 +0100

    Add red and black image and helpers for testing int color

--- src/test/scala/helpers/TestWithImages.scala
+++ src/test/scala/helpers/TestWithImages.scala
@@ -5,5 +5,7 @@ trait TestWithImages {
   def white4x4Gif: String = "src/test/scala/testFiles/4x4white.gif"
   def white4x4Jpg: String = "src/test/scala/testFiles/4x4white.jpg"
   def white4x4Png: String = "src/test/scala/testFiles/4x4white.png"
+  def black4x4Png: String = "src/test/scala/testFiles/4x4black.png"
+  def red4x4Png: String = "src/test/scala/testFiles/4x4red.png"
 
 }
---
+++ src/test/scala/helpers/TestWithIntColor.scala
@@ -0,0 +1,17 @@
+package helpers
+
+// ((what >> 16) & 0xFF).toByte,
+//      ((what >> 8) & 0xFF).toByte,
+//      (what & 0xFF).toByte)
+
+trait TestWithIntColor {
+  def getRed(int: Int): Int = (int >> 16) & 0xFF
+
+  def getGreen(int: Int): Int = (int >> 8) & 0xFF
+
+  def getBlue(int: Int): Int = int & 0xFF
+
+  def hasRgb(int: Int, r: Int, g: Int, b: Int): Boolean =
+    getRed(int) == r && getGreen(int) == g && getBlue(int) == b
+
+}
