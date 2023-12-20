ea8aa55ba48952f02058f7352952d46d7adc3980
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-20 17:29:21 +0100

    Add javadoc to IntAsColorToRgb24BitColorConverter

--- src/main/scala/asciiArtApp/converters/color/toRgb/IntAsColorToRgb24BitColorConverter.scala
+++ src/main/scala/asciiArtApp/converters/color/toRgb/IntAsColorToRgb24BitColorConverter.scala
@@ -3,6 +3,15 @@ package asciiArtApp.converters.color.toRgb
 import asciiArtApp.model.color.rgb.Rgb24BitColor
 import converters.Converter
 
+/**
+ * Converts an Int representing color into Rgb24BitColor case class.
+ *
+ * The Int has to pack the color information in this exact manner:
+ *
+ * Red: bits 16 to 23
+ * Green: bits 8 to 15
+ * Blue: bits 0 to 7
+ */
 class IntAsColorToRgb24BitColorConverter extends Converter[Int, Rgb24BitColor] {
   override def convert(what: Int): Rgb24BitColor =
     Rgb24BitColor((what >> 16) & 0xFF, (what >> 8) & 0xFF, what & 0xFF)
