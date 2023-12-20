8fe40ba854a1690ad939e2b2691bc2eb4b4db403
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-20 17:23:09 +0100

    Add javadoc to WeightedRgb24BitToGreyscale8BitColorConverter

--- src/main/scala/asciiArtApp/converters/color/toGreyscale/specific/WeightedRgb24BitToGreyscale8BitColorConverter.scala
+++ src/main/scala/asciiArtApp/converters/color/toGreyscale/specific/WeightedRgb24BitToGreyscale8BitColorConverter.scala
@@ -2,5 +2,8 @@ package asciiArtApp.converters.color.toGreyscale.specific
 
 import asciiArtApp.converters.color.toGreyscale.Rgb24BitToGreyscale8BitColorConverter
 
+/**
+ * Default weighted Rgb24Bit to Greyscale8Bit using recommended weights
+ */
 class WeightedRgb24BitToGreyscale8BitColorConverter
     extends Rgb24BitToGreyscale8BitColorConverter(0.3, 0.59, 0.11)
