6c32aa8b1f6bd4d87c40ae17f1152e09e471cb4f
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-20 17:56:22 +0100

    Add javadoc to GridImageFilter

--- src/main/scala/asciiArtApp/filters/image/grid/GridImageFilter.scala
+++ src/main/scala/asciiArtApp/filters/image/grid/GridImageFilter.scala
@@ -3,4 +3,8 @@ package asciiArtApp.filters.image.grid
 import asciiArtApp.filters.image.ImageFilter
 import asciiArtApp.model.image.grid.GridImage
 
+/**
+ * Applies to filter to given GridImage
+ * @tparam T Type of elements stored in filtered GridImage
+ */
 trait GridImageFilter[T] extends ImageFilter[GridImage[T]]
