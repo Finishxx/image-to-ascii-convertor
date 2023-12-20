88e755505555adaf4fe5b07a0f67667af1ca39e1
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-20 17:07:55 +0100

    Add javadoc to FillerGreyscale8BitToAsciiConverter

--- src/main/scala/asciiArtApp/converters/color/toAscii/default/FillerGreyscale8BitToAsciiConverter.scala
+++ src/main/scala/asciiArtApp/converters/color/toAscii/default/FillerGreyscale8BitToAsciiConverter.scala
@@ -5,14 +5,23 @@ import asciiArtApp.model.character.ascii.AsciiCharacter
 import asciiArtApp.model.color.greyscale.Greyscale8BitColor
 import converters.interval.{FillerIntervalConverter, LinearIntervalConverter}
 
-// Last element of the chars is the filler
-class FillerGreyscale8BitToAsciiConverter(chars: Seq[AsciiCharacter])
+/**
+ * Maps the whole Greyscale8Bit value range onto given characters filling the gaps
+ * with last character in characters
+ *
+ * First tries to map one greyscale value onto onto unique one character, if that is not possible
+ * maps one-to-one greyscale values from 0 to characters.size - 2 with all but last character
+ * in characters and maps rest of greyscale values to the last character.
+ *
+ * @param characters Characters which the Converter maps to
+ */
+class FillerGreyscale8BitToAsciiConverter(characters: Seq[AsciiCharacter])
     extends Greyscale8BitToAsciiConverter {
-  require(chars.length <= 256, "Please provide up to 256 values!")
-  require(chars.nonEmpty, "Please provide at least one character!")
+  require(characters.length <= 256, "Please provide up to 256 values!")
+  require(characters.nonEmpty, "Please provide at least one character!")
 
   private val fillerConverter: FillerIntervalConverter[AsciiCharacter] =
-    new FillerIntervalConverter(chars.init, chars.last, 0, 255)
+    new FillerIntervalConverter(characters.init, characters.last, 0, 255)
   override def convert(what: Greyscale8BitColor): AsciiCharacter =
     fillerConverter.convert(what.intensity)
 }
