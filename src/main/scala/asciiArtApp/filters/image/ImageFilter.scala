34e3cd85bef01669bbacb0675ec67fd7309ee59b
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-20 17:59:35 +0100

    Add javadoc to ImageFilter

--- src/main/scala/asciiArtApp/filters/image/ImageFilter.scala
+++ src/main/scala/asciiArtApp/filters/image/ImageFilter.scala
@@ -3,4 +3,8 @@ package asciiArtApp.filters.image
 import asciiArtApp.model.image.Image
 import filters.Filter
 
+/**
+ * Filters Images
+ * @tparam T Type of Image to filter
+ */
 trait ImageFilter[T <: Image] extends Filter[T]
