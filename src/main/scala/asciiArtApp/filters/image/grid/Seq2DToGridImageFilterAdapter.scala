c1fe41f60f47f253e12b4589af71f309f14910ca
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-20 17:59:09 +0100

    Add javadoc to Seq2DToGridImageFilterAdapter

--- src/main/scala/asciiArtApp/filters/image/grid/Seq2DToGridImageFilterAdapter.scala
+++ src/main/scala/asciiArtApp/filters/image/grid/Seq2DToGridImageFilterAdapter.scala
@@ -3,8 +3,14 @@ package asciiArtApp.filters.image.grid
 import asciiArtApp.model.image.grid.{GridImage, GridImageAs2DSeq}
 import filters.grid.Seq2DFilter
 
-class Seq2DToGridImageFilterAdapter[T](gridImageFilter: Seq2DFilter[T])
+/**
+ * Adapts Seq2DFilters to work on the inner grid of GridImage
+ *
+ * @param seq2DFilter Filter to be adapted to work with GridImages
+ * @tparam T Type of elements stored in filtered GridImage
+ */
+class Seq2DToGridImageFilterAdapter[T](seq2DFilter: Seq2DFilter[T])
     extends GridImageFilter[T] {
   override def filter(what: GridImage[T]): GridImage[T] =
-    new GridImageAs2DSeq(gridImageFilter.filter(what.toGrid))
+    new GridImageAs2DSeq(seq2DFilter.filter(what.toGrid))
 }
