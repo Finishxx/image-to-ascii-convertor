4069bd881bd7c73f965acf4ce4902e9d7cc4c7d4
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-20 17:32:43 +0100

    Add javadoc to InnerGridImageConverter

--- src/main/scala/asciiArtApp/converters/image/InnerGridImageConverter.scala
+++ src/main/scala/asciiArtApp/converters/image/InnerGridImageConverter.scala
@@ -3,6 +3,13 @@ package asciiArtApp.converters.image
 import asciiArtApp.model.image.grid.GridImage
 import converters.Converter
 
+/**
+ * Converts all inner elements of a GridImage[T] onto GridImage[V] using provided converter
+ *
+ * @param converter Converter which is applied to individual elements of GridImage
+ * @tparam T Inner type of GridImage[T], which we expect as input
+ * @tparam V Inner type of GridImage[V], which we convert to
+ */
 class InnerGridImageConverter[T, V](converter: Converter[T, V])
     extends Converter[GridImage[T], GridImage[V]] {
   override def convert(what: GridImage[T]): GridImage[V] =
