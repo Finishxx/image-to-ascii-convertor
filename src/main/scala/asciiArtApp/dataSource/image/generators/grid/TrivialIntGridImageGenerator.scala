e641180667753f5869fc4909e62bff7e881a7584
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-20 17:43:50 +0100

    Add javadoc to TrivialIntGridImageGenerator

--- src/main/scala/asciiArtApp/dataSource/image/generators/grid/TrivialIntGridImageGenerator.scala
+++ src/main/scala/asciiArtApp/dataSource/image/generators/grid/TrivialIntGridImageGenerator.scala
@@ -5,13 +5,20 @@ import dataSource.generators.number.IntGenerator
 
 import scala.util.Random
 
+/**
+ * Generates an Int GridImage using provided intGenerator
+ *
+ * @param width Desired width of the generated GridImage
+ * @param height Desired height of the generated GridImage
+ * @param intGenerator Generates individual ints contained in produced GridImage
+ */
 class TrivialIntGridImageGenerator(
   width: Int,
   height: Int,
   intGenerator: IntGenerator)
     extends GridImageGenerator[Int](width, height) {
-  require(width >= 0, "Negative width does not make sense for a grid!")
-  require(height >= 0, "Negative height does not make sense for a grid!")
+  require(width > 0, "Non-positive width does not make sense for a grid!")
+  require(height > 0, "Non-positive height does not make sense for a grid!")
   override def generate(width: Int, height: Int): GridImage[Int] =
     new GridImageAs2DSeq(Seq.fill(width, height)(intGenerator.provide()))
 }
