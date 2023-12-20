a4231666a6f0e4abd12f278ec8e3894a16e2db7d
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-20 17:49:56 +0100

    Add javadoc to RotateGridImageFilter

--- src/main/scala/asciiArtApp/filters/image/grid/rotate/RotateGridImageFilter.scala
+++ src/main/scala/asciiArtApp/filters/image/grid/rotate/RotateGridImageFilter.scala
@@ -3,5 +3,12 @@ package asciiArtApp.filters.image.grid.rotate
 import asciiArtApp.filters.image.grid.Seq2DToGridImageFilterAdapter
 import filters.grid.rotate.RotateSeq2DFilter
 
+/**
+ * Rotates GridImage by multiples of 90 degrees
+ *
+ * @param degrees By how many degrees should the GridImage be rotated by.
+ *                Only multiples of 90 are allowed
+ * @tparam T Type of elements the GridImage stores
+ */
 class RotateGridImageFilter[T](degrees: Int)
     extends Seq2DToGridImageFilterAdapter[T](new RotateSeq2DFilter[T](degrees))
