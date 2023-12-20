ec9faeb3059140de80ec8560e3e8b48dfae02fcc
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-20 17:44:31 +0100

    Add javadoc to ImageGenerator

--- src/main/scala/asciiArtApp/dataSource/image/generators/ImageGenerator.scala
+++ src/main/scala/asciiArtApp/dataSource/image/generators/ImageGenerator.scala
@@ -3,4 +3,8 @@ package asciiArtApp.dataSource.image.generators
 import asciiArtApp.model.image.Image
 import dataSource.generators.Generator
 
+/**
+ * Superclass of Generators, which generate Images
+ * @tparam T Type of the Image
+ */
 trait ImageGenerator[T <: Image] extends Generator[T]
