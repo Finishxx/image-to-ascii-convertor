6761403cc6bfe27cebd002594cb0b9b3ad083556
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-20 17:47:31 +0100

    Add javadoc to FlipGridImageFilter

--- src/main/scala/asciiArtApp/filters/image/grid/flip/FlipGridImageFilter.scala
+++ src/main/scala/asciiArtApp/filters/image/grid/flip/FlipGridImageFilter.scala
@@ -4,5 +4,11 @@ import asciiArtApp.filters.image.grid.Seq2DToGridImageFilterAdapter
 import filters.grid.flip.Axis.Axis
 import filters.grid.flip.FlipSeq2DFilter
 
+/**
+ * Flips given GridImage around given axis
+ *
+ * @param axis Axis which the GridImage will be around on, either X or Y
+ * @tparam T Type contained in the GridImage we desire to flip
+ */
 class FlipGridImageFilter[T](axis: Axis)
     extends Seq2DToGridImageFilterAdapter[T](new FlipSeq2DFilter[T](axis))
