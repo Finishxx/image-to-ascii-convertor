27777c8fadd767eb73bd2832ce45ad89006e65e6
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-20 17:42:11 +0100

    Add javadoc to GridImageGenerator

--- src/main/scala/asciiArtApp/dataSource/image/generators/grid/GridImageGenerator.scala
+++ src/main/scala/asciiArtApp/dataSource/image/generators/grid/GridImageGenerator.scala
@@ -3,6 +3,13 @@ package asciiArtApp.dataSource.image.generators.grid
 import asciiArtApp.dataSource.image.generators.ImageGenerator
 import asciiArtApp.model.image.grid.GridImage
 
+/**
+ * Generates a GridImage[T] with given width and height
+ *
+ * @param width Desired width of the generated GridImage
+ * @param height Desired height of the generated GridImage
+ * @tparam T Type contained in individual pixels of GridImage
+ */
 abstract class GridImageGenerator[T](width: Int, height: Int)
     extends ImageGenerator[GridImage[T]] {
   protected def generate(width: Int, height: Int): GridImage[T]
