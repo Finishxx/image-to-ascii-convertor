6598759d9bf685165911af5d7c9ec8042330441a
Martin Tománek <tomanma9@fit.cvut.cz>
2023-12-20 17:13:18 +0100

    Add javadoc to LinearGreyscale8BitToAsciiConverter

--- src/main/scala/asciiArtApp/converters/color/toAscii/default/LinearGreyscale8BitToAsciiConverter.scala
+++ src/main/scala/asciiArtApp/converters/color/toAscii/default/LinearGreyscale8BitToAsciiConverter.scala
@@ -5,11 +5,20 @@ import asciiArtApp.model.character.ascii.AsciiCharacter
 import asciiArtApp.model.color.greyscale.Greyscale8BitColor
 import converters.interval.LinearIntervalConverter
 
-class LinearGreyscale8BitToAsciiConverter(chars: Seq[AsciiCharacter])
+/**
+ * Maps the whole Greyscale8Bit value range onto given characters arguably as equally as possible
+ *
+ * If characters can't be mapped truly equally, meaning we can not map equal number
+ * of greyscale values to each individual character, maps more greyscale values
+ * onto first character and divides the rest equally.
+ *
+ * @param characters Characters which the Converter maps to
+ */
+class LinearGreyscale8BitToAsciiConverter(characters: Seq[AsciiCharacter])
     extends Greyscale8BitToAsciiConverter {
 
   private val linearConverter: LinearIntervalConverter[AsciiCharacter] =
-    new LinearIntervalConverter(chars, 0, 255)
+    new LinearIntervalConverter(characters, 0, 255)
   override def convert(what: Greyscale8BitColor): AsciiCharacter =
     linearConverter.convert(what.intensity)
 }
